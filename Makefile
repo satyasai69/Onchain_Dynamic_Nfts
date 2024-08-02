a5a23d7eb1e0ef6bb46346162869d03e69fc047a
satyasai69 <ch.satya.sai133@gmail.com>
2024-08-02 18:20:39 +0530

    MoodNft

--- Makefile
+++ Makefile
@@ -11,16 +11,16 @@ ifeq ($(findstring --network sepolia,$(ARGS)),--network sepolia)
 endif
 
 deploy:
-	@forge script script/DeployBasicNft.s.sol:DeployBasicNft $(NETWORK_ARGS)
+	@forge script script/DeployMoodNft.s.sol:DepolyMoodNft $(NETWORK_ARGS)
 
 
-deploy-anvil: ; forge script ./script/DeployBasicNft.s.sol:DeployBasicNft --rpc-url ${ANVIL_RPC_URL} --private-key ${DEFAULT_ANVIL_KEY} --broadcast -vvvvv
+deploy-anvil: ; forge script ./script/DeployMoodNft.s.sol:DepolyMoodNft --rpc-url ${ANVIL_RPC_URL} --private-key ${DEFAULT_ANVIL_KEY} --broadcast -vvvvv
 
-deploy-sepolia: ; forge script ./script/DeployBasicNft.s.sol:DeployBasicNft --rpc-url ${SEPOLIA_RPC_URL} --private-key ${PRIVATE_KEY} --broadcast  --verify --etherscan-api-key ${ETHERSCAN_API_KEY} -vvvvv
+deploy-sepolia: ; forge script ./script/DeployMoodNft.s.sol:DepolyMoodNft --rpc-url ${SEPOLIA_RPC_URL} --private-key ${PRIVATE_KEY} --broadcast  --verify --etherscan-api-key ${ETHERSCAN_API_KEY} -vvvvv
 
-mintNft-anvil: ; forge script ./script/interactions.s.sol:MintBasicNft --rpc-url  ${ANVIL_RPC_URL} --private-key ${DEFAULT_ANVIL_KEY} --broadcast -vvvvv
+mintNft-anvil: ; forge script ./script/interactions.s.sol:MintMoodNft --rpc-url  ${ANVIL_RPC_URL} --private-key ${DEFAULT_ANVIL_KEY} --broadcast -vvvvv
 
 
-mintNft-sepolia: ; forge script ./script/interactions.s.sol:MintBasicNft --rpc-url  ${SEPOLIA_RPC_URL} --private-key ${PRIVATE_KEY} --broadcast -vvvvv
+mintNft-sepolia: ; forge script ./script/interactions.s.sol:MintMoodNft --rpc-url  ${SEPOLIA_RPC_URL} --private-key ${PRIVATE_KEY} --broadcast -vvvvv
 
 
--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -3,19 +3,82 @@
 pragma solidity ^0.8.19;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
-contract MoodNfts is ERC721 {
+contract MoodNft is ERC721 {
+    error MoodNft_CantFilpMoodIfNotOwner();
+
     uint256 private s_tokenCounter;
     string private s_sadSvg;
     string private s_happySvg;
-    mapping(uint256 => string) public tokenURIs;
+
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 => Mood) private tokenIdToMood;
 
     constructor(
-        string memory sadsvg,
-        string memory happysvg
+        string memory happysvg,
+        string memory sadsvg
     ) ERC721("MoodNfts", "MNFTs") {
         s_tokenCounter = 0;
         s_sadSvg = sadsvg;
         s_happySvg = happysvg;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
+        s_tokenCounter++;
+    }
+
+    function filpMood(uint256 tokenId) public {
+        if (
+            getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender
+        ) {
+            revert MoodNft_CantFilpMoodIfNotOwner();
+        }
+        if (tokenIdToMood[tokenId] == Mood.HAPPY) {
+            tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageURL;
+
+        if (tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageURL = s_happySvg;
+        } else {
+            imageURL = s_sadSvg;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes( // bytes casting actually unnecessary as 'abi.encodePacked()' returns a bytes
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                                '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                                imageURL,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
